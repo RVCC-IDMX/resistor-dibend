1f07cd7c57b0b8e4ce150497522fe638c6bb9045
David DiBenedetto <david@njweb.solutions>
2022-11-24 11:38:15 -0700

    update band1 colors onclick
    Add onclick function to each band1 color to set band1 color

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import { getResistorOhms } from './resistor.js';
+//import { getResistorOhms } from './resistor.js';
+const band1Div = document.getElementById('color0');
+const band2Div = document.getElementById('color1');
+const multiplierDiv = document.getElementById('color2');
+const toleranceDiv = document.getElementById('color3');
+const band1 = document.getElementById('b0');
+const band2 = document.getElementById('b1');
+const multiplier = document.getElementById('b2');
+const tolerance = document.getElementById('b3');
+[].forEach.call(band1Div.childNodes, (child) => {
+  child.onclick = () => {
+    band1.className = 'box ' + child.innerText.toLowerCase();
+  }
+});
